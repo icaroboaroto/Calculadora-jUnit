1d4f8a2a36125c1b8a91b8cacc7fda79dfa98bcd
Ícaro Fontanive <92635296+icaroboaroto@users.noreply.github.com>
2023-09-22 22:13:53 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/calculadoraa/Calculadora.java
@@ -0,0 +1,21 @@
+
+package com.mycompany.calculadoraa;
+
+
+public class Calculadora {
+     public int somar(int a, int b){
+        return a + b;
+    }
+    
+    public int sub(int a, int b){
+        return a - b;
+    }
+    
+    public int mult(int a, int b){
+        return a * b;    
+    }
+    
+    public int div(int a, int b){
+        return a / b;
+    }
+}
---
+++ src/main/java/com/mycompany/calculadoraa/Calculadoraa.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.calculadoraa;
+
+public class Calculadoraa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
